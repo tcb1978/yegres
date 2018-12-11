c2ab0f36e28cacba857552974a6d262a859717ab
Matthew Eldredge <mattheweldredge@gmail.com>
2018-12-10 19:23:04 -0800

    init

---
+++ src/js/imported-example.js
@@ -0,0 +1,5 @@
+export class ImportedClass {
+	doesItWork() {
+		console.log('imports work!');
+	}
+}
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import { ImportedClass } from './imported-example.js';
+console.log('main works')
+
+const testing = new ImportedClass();
+
+testing.doesItWork();
